1bb3dd332825e3f04a0db135885ddccf0be749ed
dirkgentlyhhgg@gmail.com <dirkgentlyhhgg@gmail.com>
2023-12-11 15:56:16 +0000

    Edited settings and Form1.News.ArticleEdit

--- client_code/Form1/News/ArticleEdit/__init__.py
+++ client_code/Form1/News/ArticleEdit/__init__.py
@@ -9,5 +9,6 @@ class ArticleEdit(ArticleEditTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
+    self.categories = [(cat['name'], cat) for cat in app_tables.categories.search()]
+    self.category_box.items = self.categories
