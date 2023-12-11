77c2b8af811812a1cdd806b93cf6faf24b8bbaa5
dirkgentlyhhgg@gmail.com <dirkgentlyhhgg@gmail.com>
2023-12-11 21:23:16 +0000

    Edited Form1.News, ServerModule1 and Form1.News.ArticleView

--- client_code/Form1/News/ArticleView/__init__.py
+++ client_code/Form1/News/ArticleView/__init__.py
@@ -1,9 +1,11 @@
 from ._anvil_designer import ArticleViewTemplate
+
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..ArticleEdit import ArticleEdit
 
 class ArticleView(ArticleViewTemplate):
   def __init__(self, **properties):
@@ -11,3 +13,6 @@ class ArticleView(ArticleViewTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def edit_article_button_click(self, **event_args):
+    pass
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -32,3 +32,19 @@ def add_article(article_dict):
     created=datetime.now(),
     **article_dict
   )
+
+@anvil.server.callable
+def get_articles():
+  # Get a list of articles from the Data Table, sorted by 'created' column, in descending order
+  return app_tables.articles.search(
+    tables.order_by("created", ascending=False)
+  )
+
+@anvil.server.callable
+def update_article(article, article_dict):
+  # check that the article given is really a row in the ‘articles’ table
+  if app_tables.articles.has_row(article):
+    article_dict['updated'] = datetime.now()
+    article.update(**article_dict)
+  else:
+    raise Exception("Article does not exist")
